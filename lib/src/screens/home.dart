c18827c8fb8e290bc18523031b33e12aedfb4de0
Saikat Halder <saikat@itobuz.com>
2019-02-05 18:10:01 +0530

    added google map
    Signed-off-by: Saikat Halder <saikat@itobuz.com>

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../components/header.dart';
 import '../components/navbar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  HomeScreenState createState() {
+    return new HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(title: 'Home'),
       drawer: NavBar(),
-      body: Text('Home Screen'),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        options: GoogleMapOptions(
+          cameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
